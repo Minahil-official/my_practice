87dcaa468a9afa49a31549d7a1a003ef689967bd
Minahil Nawaz <minahilnawaz98@gmail.com>
2024-11-07 02:22:40 +0500

    python practice

--- functions.py
+++ functions.py
@@ -1,28 +1,28 @@
 ## simple function
-# def even_odd(x):
-#        if x % 2 ==0:
-#           print('EVEN')
-#        else:
-#         print("ODD")
-# even_odd(int(input("enter your number:")))
+def even_odd(x):
+       if x % 2 ==0:
+          print('EVEN')
+       else:
+        print("ODD")
+even_odd(int(input("enter your number:")))
 
 #add function
-# def sum(a,b):  #here a and b are parameters
-#     print(a+b)
-# sum(3,5) #here 3 and 5 are arguments
+def sum(a,b):  #here a and b are parameters
+    print(a+b)
+sum(3,5) #here 3 and 5 are arguments
 
 #FUNCTION WITH RETURN
 
-# def sum(a,b): 
-#     return(a+b) #code distructor
-# print(sum(3,5)) 
+def sum(a,b): 
+    return(a+b) #code distructor
+print(sum(3,5)) 
 
 
 #TYPES OF ARGUMENTS
 # DEFUALT ARGUMENTS
-# def sum(a,b=3): 
-#     return(a+b) 
-# print(sum(3)) 
+def sum(a,b=3): 
+    return(a+b) 
+print(sum(3)) 
 
 
 #positional arguments
